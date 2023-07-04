592022403096489ca2a67b76c3a12c90d05f1422
BaeCheolHan <joker10421@gmail.com>
2023-07-04 15:41:24 +0900

    라인 정렬

--- src/main/java/com/my/stock/stockmanager/utils/KisTokenProvider.java
+++ src/main/java/com/my/stock/stockmanager/utils/KisTokenProvider.java
@@ -14,18 +14,14 @@ import java.util.HashMap;
 public class KisTokenProvider {
 
 	private final String accessTokenGenerateUrl;
-
 	private final String appKey;
-
 	private final String appSecret;
-
-
 	private static KisToken token;
 
 	public KisTokenProvider(@Value("${api.kis.access-token-generate-url}") String accessTokenGenerateUrl,
 							@Value("${api.kis.appKey}") String appKey,
 							@Value("${api.kis.app-secret}") String appSecret
-							 ) throws Exception {
+	) throws Exception {
 		this.appKey = appKey;
 		this.accessTokenGenerateUrl = accessTokenGenerateUrl;
 		this.appSecret = appSecret;
@@ -38,12 +34,11 @@ public class KisTokenProvider {
 				.readValue(ApiCaller.getInstance().post(accessTokenGenerateUrl, param), KisToken.class);
 	}
 
-
 	public KisToken getToken() throws Exception {
 		LocalDateTime now = LocalDateTime.now();
 		LocalDateTime expired = LocalDateTime.parse(token.getAccess_token_token_expired(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 
-		if(expired.isBefore(now)) {
+		if (expired.isBefore(now)) {
 			HashMap<String, Object> param = new HashMap<>();
 			param.put("grant_type", "client_credentials");
 			param.put("appkey", appKey);
