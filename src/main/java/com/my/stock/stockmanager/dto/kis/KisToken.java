a71214c42275fd6cf7c71baf6565cf48b3b6c783
BaeCheolHan <joker10421@gmail.com>
2023-07-04 15:40:38 +0900

    kis api 연동 전 token 제공자 구현

---
+++ src/main/java/com/my/stock/stockmanager/dto/kis/KisToken.java
@@ -0,0 +1,13 @@
+package com.my.stock.stockmanager.dto.kis;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class KisToken {
+	private String access_token;
+	private String access_token_token_expired;
+	private String token_type;
+	private int expires_in;
+}
