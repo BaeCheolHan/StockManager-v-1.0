36c20d67a050d5ba56d1297d1dc3cd9f4e5f62e0
BaeCheolHan <joker10421@gmail.com>
2023-07-07 16:49:12 +0900

    stock 정렬 변경

--- src/main/java/com/my/stock/stockmanager/service/StockService.java
+++ src/main/java/com/my/stock/stockmanager/service/StockService.java
@@ -5,15 +5,18 @@ import com.my.stock.stockmanager.dto.stock.DashboardStock;
 import com.my.stock.stockmanager.dto.stock.request.StockSaveRequest;
 import com.my.stock.stockmanager.exception.StockManagerException;
 import com.my.stock.stockmanager.rdb.entity.BankAccount;
+import com.my.stock.stockmanager.rdb.entity.ExchangeRate;
 import com.my.stock.stockmanager.rdb.entity.Stock;
 import com.my.stock.stockmanager.rdb.repository.BankAccountRepository;
-import com.my.stock.stockmanager.rdb.repository.MemberRepository;
+import com.my.stock.stockmanager.rdb.repository.ExchangeRateRepository;
 import com.my.stock.stockmanager.rdb.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -21,14 +24,25 @@ public class StockService {
 
 	private final StockRepository stockRepository;
 
+	private final ExchangeRateRepository exchangeRateRepository;
+
 	private final BankAccountRepository bankAccountRepository;
 
 	public List<DashboardStock> getStocks(Long memberId, Long bankId) {
 		List<DashboardStock> stocks = stockRepository.findAllDashboardStock(memberId, bankId);
-		final double totalInvestmentAmount = stocks.stream().mapToDouble(it ->it.getAvgPrice() * it.getQuantity()).sum();
+		List<ExchangeRate> exchangeRateList = exchangeRateRepository.findAll();
+
+		final double totalInvestmentAmount = stocks.stream()
+				.mapToDouble(it -> {
+					if (!it.getNational().equals("KR")) {
+						return it.getAvgPrice() * it.getQuantity() * exchangeRateList.get(exchangeRateList.size() -1).getBasePrice();
+					} else {
+						return it.getAvgPrice() * it.getQuantity();
+					}
+				}).sum();
 
 		stocks.forEach(stock -> stock.setPriceImportance((stock.getAvgPrice() * stock.getQuantity()) / totalInvestmentAmount * 100.0));
-		return stocks;
+		return stocks.stream().sorted(Comparator.comparing(DashboardStock::getPriceImportance).reversed()).collect(Collectors.toList());
 	}
 
 	@Transactional
