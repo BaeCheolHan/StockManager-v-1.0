4bc744423fb8a1a70365bd37f106639a8a402d66
vendys-chulhan <chulhan@ventys.co.kr>
2024-10-21 13:30:29 +0900

    fix bug

--- src/main/java/com/my/stock/stockmanager/dto/exchangerate/request/ExchangeRateApiRequest.java
+++ src/main/java/com/my/stock/stockmanager/dto/exchangerate/request/ExchangeRateApiRequest.java
@@ -14,12 +14,13 @@ public class ExchangeRateApiRequest {
 
     private String searchdate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
 
+
     public ExchangeRateApiRequest minusDay(long days) {
         this.searchdate = LocalDateTime.now().minusDays(days).format(DateTimeFormatter.ofPattern("yyyyMMdd"));
         return this;
     }
 
     public String getUriQuery() {
-        return String.format("data=%s&authkey=%s&searchdate=", data, authkey, searchdate);
+        return String.format("data=%s&authkey=%s&searchdate=%s", data, authkey, searchdate);
     }
 }
